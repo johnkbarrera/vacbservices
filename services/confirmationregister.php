a81e0837e5c20676debb88eb968e259e0faf3a86
john kevin <johnkevinbarrera@gmail.com>
2019-06-06 10:57:44 -0500

    v2

--- services/confirmationregister.php
+++ services/confirmationregister.php
@@ -3,42 +3,58 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 	$email = $_POST['email'];
-	$password = $_POST['code'];
-	//$password = password_hash($password, PASSWORD_DEFAULT);
-	$password = sha1($password);
-	$user = $email;
+	$code = $_POST['code'];
 
 	require_once '../DAO/connection.php';
 
-	//generar aleatorio
-	$cod_conf = "123456";
-	// Enviar email
 
-	// Verificamos si el correo ya fue registrado
+	// ExTRAEMOS EN CODIGO DE LA BD
+	$cod_conf = "";
+	$sql = "SELECT cod_conf FROM usuario WHERE correo='".$email."'";
+	$executed0 = pg_query($conn, $sql);
+	while ($line = pg_fetch_array($executed0, null, PGSQL_ASSOC)) {
+		$cod_conf = $line['cod_conf'];
+	}
 
+	// VERIFICAMOS SI EL CODIGO ES CORRECTO
+	if ($code == $cod_conf) {
 
-	$sql = "INSERT INTO usuario(usuario, contrasenia, correo, estado, cod_conf) 
-			VALUES (
-				'".$user."',
-				'".$password."',
-				'".$email."',
-				false,
-				'".$cod_conf."'
-			)";
+		// VALIDAMOS A EL USUARIO - modificamos estado
+		$sql = "UPDATE public.usuario
+				SET estado=true, cod_conf='000000'
+				WHERE correo='".$email."';";
+		$executed1 = pg_query($conn, $sql);
 
-	$executed = pg_query($conn, $sql);
+		// CREAMOS UN GANADERO
+		if ($executed1){
+			$sql = "INSERT INTO public.ganadero(usuario, detalle)
+				VALUES ('".$email."', 'detalle');";
+			$executed2 = pg_query($conn, $sql);
 
-	if ($executed) {
-		$result["success"] = "1";
-		$result["message"] = "success";
-		
-		echo json_encode($result);
-		pg_close($conn);
+			if ($executed2){
+				$result["success"] = "1";
+				$result["message"] = "Código verificado";
+
+				echo json_encode($result);
+				pg_close($conn);
+			} else {
+				$result["success"] = "0";
+				$result["message"] = "No pudimos crear ganadero";
+				echo json_encode($result);
+				pg_close($conn);
+			}
+
+		} else {
+			$result["success"] = "0";
+			$result["message"] = "No pudimos actualizar el estado de usuario";
+			echo json_encode($result);
+			pg_close($conn);
+		}
 
 	} else {
 		$result["success"] = "0";
-		$result["message"] = "error";
-
+		$result["message"] = "Código incorrecto, ingrese otro código";
+		
 		echo json_encode($result);
 		pg_close($conn);
 
